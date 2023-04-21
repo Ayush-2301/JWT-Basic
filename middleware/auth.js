b89933f00e92f93340a871194dce98dfbf1a2034
Ayush <ayushchauhan233@gmail.com>
2023-04-21 17:15:27 +0530

    JWT-Basic compeleted

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const authorizationMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const { id, username } = decoded;
+    req.user = { id, username };
+    next();
+  } catch (error) {
+    throw new CustomAPIError("not authorized to access this route", 401);
+  }
+};
+module.exports = authorizationMiddleware;
